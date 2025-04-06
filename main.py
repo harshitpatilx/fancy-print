480e94381b86e9d6809e77373783c88f3071c15b
Harshit <mailme.harshitp@gmail.com>
2025-04-06 15:25:56 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import time
+import random
+
+def prinj(string):
+    bk = ''
+    for i in string:
+        while True:
+            time.sleep(0.03)
+            r = random.randint(32, 125)
+            rn = chr(r)
+            print(bk, rn, sep='')
+            if rn == i:
+                bk+=rn
+                break
+
+a = input(" Enter")
+prinj(a)
+time.sleep(5)
